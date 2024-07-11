19f20295bc37470f8234f35ec9251e7ee73a40c3
Jonathan Carlo <jonathancarlo20@gmail.com>
2024-07-11 23:09:56 +0700

    Third commit

---
+++ Hackathon-app/app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className = "justify-center items-center w-full h-full">
+      <Text className = "text-3xl ">index</Text>
+    </View>
+  )
+}
+
+export default index
